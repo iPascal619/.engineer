ecae9ac84fdc3269e64359390a02dab870288fb7
Chukwuma pascal Onuoha <c.onuoha@alustudent.com>
2025-09-08 09:58:56 +0200

    Update email card to open Gmail compose window

--- apps/landing/src/views/HomePage.vue
+++ apps/landing/src/views/HomePage.vue
@@ -199,7 +199,7 @@
             </div>
             
             <div class="contact-grid">
-              <a href="mailto:pascalonuoha324@gmail.com" class="contact-card email-card">
+              <a href="https://mail.google.com/mail/?view=cm&fs=1&to=pascalonuoha324@gmail.com&su=Hello%20Pascal!&body=Hi%20Pascal,%0A%0AI%20found%20your%20portfolio%20and%20would%20like%20to%20connect..." target="_blank" class="contact-card email-card">
                 <div class="contact-icon">
                   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M4 4H20C21.1 4 22 4.9 22 6V18C22 19.1 21.1 20 20 20H4C2.9 20 2 19.1 2 18V6C2 4.9 2.9 4 4 4Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
